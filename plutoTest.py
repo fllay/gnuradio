9f73872b584add1929810ed16f56c58d2f000f7b
fllay <calgalli@gmail.com>
2023-07-04 15:09:24 +0700

    sep tx rx qpsk

--- plutoTest.py
+++ plutoTest.py
@@ -11,13 +11,11 @@
 from packaging.version import Version as StrictVersion
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio import analog
 from gnuradio import blocks
 import numpy
 from gnuradio import digital
-from gnuradio import filter
-from gnuradio.filter import firdes
 from gnuradio import gr
+from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
@@ -73,7 +71,7 @@ class plutoTest(gr.top_block, Qt.QWidget):
         self.mod_obj = mod_obj = digital.constellation_qpsk().base()
         self.excess_bw = excess_bw = .350
         self.timing_loop_bw = timing_loop_bw = 6.28/100.0
-        self.rrc_taps = rrc_taps = firdes.root_raised_cosine(1.0,samp_rate,samp_rate/sps,excess_bw,11*sps)
+        self.rrc_taps = rrc_taps = firdes.root_raised_cosine(1.0,samp_rate,samp_rate/sps,excess_bw,21*sps)
         self.freq = freq = 880000000
         self.CMA = CMA = digital.adaptive_algorithm_cma( mod_obj, .0001, 4).base()
         self.BW = BW = 200000
@@ -85,19 +83,6 @@ class plutoTest(gr.top_block, Qt.QWidget):
         self._timing_loop_bw_range = Range(0.0, 0.2, 0.01, 6.28/100.0, 200)
         self._timing_loop_bw_win = RangeWidget(self._timing_loop_bw_range, self.set_timing_loop_bw, "Time: BW", "slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._timing_loop_bw_win)
-        self.soapy_rtlsdr_source_0 = None
-        dev = 'driver=rtlsdr'
-        stream_args = ''
-        tune_args = ['']
-        settings = ['']
-
-        self.soapy_rtlsdr_source_0 = soapy.source(dev, "fc32", 1, '',
-                                  stream_args, tune_args, settings)
-        self.soapy_rtlsdr_source_0.set_sample_rate(0, samp_rate)
-        self.soapy_rtlsdr_source_0.set_gain_mode(0, False)
-        self.soapy_rtlsdr_source_0.set_frequency(0, freq)
-        self.soapy_rtlsdr_source_0.set_frequency_correction(0, 0)
-        self.soapy_rtlsdr_source_0.set_gain(0, 'TUNER', 50)
         self.soapy_plutosdr_sink_0 = None
         dev = 'driver=plutosdr'
         stream_args = ''
@@ -109,7 +94,7 @@ class plutoTest(gr.top_block, Qt.QWidget):
         self.soapy_plutosdr_sink_0.set_sample_rate(0, samp_rate)
         self.soapy_plutosdr_sink_0.set_bandwidth(0, 500000)
         self.soapy_plutosdr_sink_0.set_frequency(0, freq)
-        self.soapy_plutosdr_sink_0.set_gain(0, min(max(45, 0.0), 89.0))
+        self.soapy_plutosdr_sink_0.set_gain(0, min(max(60, 0.0), 89.0))
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -201,119 +186,6 @@ class plutoTest(gr.top_block, Qt.QWidget):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(1, 2):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self.qtgui_const_sink_x_0_0_0 = qtgui.const_sink_c(
-            1024, #size
-            "Costas loop IQ", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_const_sink_x_0_0_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0_0_0.set_y_axis((-2), 2)
-        self.qtgui_const_sink_x_0_0_0.set_x_axis((-2), 2)
-        self.qtgui_const_sink_x_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
-        self.qtgui_const_sink_x_0_0_0.enable_autoscale(True)
-        self.qtgui_const_sink_x_0_0_0.enable_grid(False)
-        self.qtgui_const_sink_x_0_0_0.enable_axis_labels(True)
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "red", "red", "red",
-            "red", "red", "red", "red", "red"]
-        styles = [0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0]
-        markers = [0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_const_sink_x_0_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_const_sink_x_0_0_0.set_line_label(i, labels[i])
-            self.qtgui_const_sink_x_0_0_0.set_line_width(i, widths[i])
-            self.qtgui_const_sink_x_0_0_0.set_line_color(i, colors[i])
-            self.qtgui_const_sink_x_0_0_0.set_line_style(i, styles[i])
-            self.qtgui_const_sink_x_0_0_0.set_line_marker(i, markers[i])
-            self.qtgui_const_sink_x_0_0_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_const_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0_0_0.qwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_0_0_win, 1, 0, 1, 1)
-        for r in range(1, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self.qtgui_const_sink_x_0_0 = qtgui.const_sink_c(
-            1024, #size
-            "Time Sync IQ", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_const_sink_x_0_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0_0.set_y_axis((-2), 2)
-        self.qtgui_const_sink_x_0_0.set_x_axis((-2), 2)
-        self.qtgui_const_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
-        self.qtgui_const_sink_x_0_0.enable_autoscale(True)
-        self.qtgui_const_sink_x_0_0.enable_grid(False)
-        self.qtgui_const_sink_x_0_0.enable_axis_labels(True)
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "red", "red", "red",
-            "red", "red", "red", "red", "red"]
-        styles = [0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0]
-        markers = [0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_const_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_const_sink_x_0_0.set_line_label(i, labels[i])
-            self.qtgui_const_sink_x_0_0.set_line_width(i, widths[i])
-            self.qtgui_const_sink_x_0_0.set_line_color(i, colors[i])
-            self.qtgui_const_sink_x_0_0.set_line_style(i, styles[i])
-            self.qtgui_const_sink_x_0_0.set_line_marker(i, markers[i])
-            self.qtgui_const_sink_x_0_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_const_sink_x_0_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0_0.qwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_0_win, 1, 1, 1, 1)
-        for r in range(1, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(1, 2):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self.low_pass_filter_0 = filter.fir_filter_ccf(
-            1,
-            firdes.low_pass(
-                1,
-                samp_rate,
-                250000,
-                100000,
-                window.WIN_HAMMING,
-                6.76))
-        self.digital_symbol_sync_xx_0 = digital.symbol_sync_cc(
-            digital.TED_ZERO_CROSSING,
-            sps,
-            timing_loop_bw,
-            0.707,
-            2.5,
-            1.5,
-            sps,
-            digital.constellation_qpsk().base(),
-            digital.IR_PFB_MF,
-            2,
-            rrc_taps)
-        self.digital_linear_equalizer_0 = digital.linear_equalizer(15, sps, CMA, True, [ ], 'corr_est')
-        self.digital_costas_loop_cc_0 = digital.costas_loop_cc(timing_loop_bw, 4, False)
         self.digital_constellation_modulator_0 = digital.generic_mod(
             constellation=mod_obj,
             differential=True,
@@ -325,25 +197,16 @@ class plutoTest(gr.top_block, Qt.QWidget):
             truncate=False)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(0.5)
         self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 255, 10000))), True)
-        self.analog_agc2_xx_0 = analog.agc2_cc((1e-1), (1e-2), 1.0, 1.0)
-        self.analog_agc2_xx_0.set_max_gain(65536)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_agc2_xx_0, 0), (self.digital_symbol_sync_xx_0, 0))
         self.connect((self.analog_random_source_x_0, 0), (self.digital_constellation_modulator_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.soapy_plutosdr_sink_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.qtgui_const_sink_x_0_0_1_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.digital_costas_loop_cc_0, 0), (self.qtgui_const_sink_x_0_0_0, 0))
-        self.connect((self.digital_linear_equalizer_0, 0), (self.digital_costas_loop_cc_0, 0))
-        self.connect((self.digital_linear_equalizer_0, 0), (self.qtgui_const_sink_x_0_0, 0))
-        self.connect((self.digital_symbol_sync_xx_0, 0), (self.digital_linear_equalizer_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.analog_agc2_xx_0, 0))
-        self.connect((self.soapy_rtlsdr_source_0, 0), (self.low_pass_filter_0, 0))
 
 
     def closeEvent(self, event):
@@ -359,18 +222,16 @@ class plutoTest(gr.top_block, Qt.QWidget):
 
     def set_sps(self, sps):
         self.sps = sps
-        self.set_rrc_taps(firdes.root_raised_cosine(1.0,self.samp_rate,self.samp_rate/self.sps,self.excess_bw,11*self.sps))
+        self.set_rrc_taps(firdes.root_raised_cosine(1.0,self.samp_rate,self.samp_rate/self.sps,self.excess_bw,21*self.sps))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.set_rrc_taps(firdes.root_raised_cosine(1.0,self.samp_rate,self.samp_rate/self.sps,self.excess_bw,11*self.sps))
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 250000, 100000, window.WIN_HAMMING, 6.76))
+        self.set_rrc_taps(firdes.root_raised_cosine(1.0,self.samp_rate,self.samp_rate/self.sps,self.excess_bw,21*self.sps))
         self.qtgui_freq_sink_x_0.set_frequency_range(0, (self.samp_rate/2))
         self.soapy_plutosdr_sink_0.set_sample_rate(0, self.samp_rate)
-        self.soapy_rtlsdr_source_0.set_sample_rate(0, self.samp_rate)
 
     def get_mod_obj(self):
         return self.mod_obj
@@ -383,15 +244,13 @@ class plutoTest(gr.top_block, Qt.QWidget):
 
     def set_excess_bw(self, excess_bw):
         self.excess_bw = excess_bw
-        self.set_rrc_taps(firdes.root_raised_cosine(1.0,self.samp_rate,self.samp_rate/self.sps,self.excess_bw,11*self.sps))
+        self.set_rrc_taps(firdes.root_raised_cosine(1.0,self.samp_rate,self.samp_rate/self.sps,self.excess_bw,21*self.sps))
 
     def get_timing_loop_bw(self):
         return self.timing_loop_bw
 
     def set_timing_loop_bw(self, timing_loop_bw):
         self.timing_loop_bw = timing_loop_bw
-        self.digital_costas_loop_cc_0.set_loop_bandwidth(self.timing_loop_bw)
-        self.digital_symbol_sync_xx_0.set_loop_bandwidth(self.timing_loop_bw)
 
     def get_rrc_taps(self):
         return self.rrc_taps
@@ -405,7 +264,6 @@ class plutoTest(gr.top_block, Qt.QWidget):
     def set_freq(self, freq):
         self.freq = freq
         self.soapy_plutosdr_sink_0.set_frequency(0, self.freq)
-        self.soapy_rtlsdr_source_0.set_frequency(0, self.freq)
 
     def get_CMA(self):
         return self.CMA
